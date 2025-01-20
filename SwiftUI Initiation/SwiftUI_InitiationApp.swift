3fb3707adedfe1e0c888c453ce93e17b1acb5262
SimonGOY <simon.goy@etu.univ-smb.fr>
2025-01-20 09:01:45 +0100

    Initial Commit

---
+++ SwiftUI Initiation/SwiftUI_InitiationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_InitiationApp.swift
+//  SwiftUI Initiation
+//
+//  Created by Simon GOY on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_InitiationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
