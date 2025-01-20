bbba66a24e28f5d2d814bafb940a4c6c0feffecf
SimonGOY <simon.goy@etu.univ-smb.fr>
2025-01-20 09:18:41 +0100

    2 - App basic : Exercice 1

---
+++ Answers.md
@@ -0,0 +1,8 @@
+#  Réponses Initiation SwiftUI
+
+## 2 - App Basic
+
+### Exercice 1
+
+Il manque un identifiant pour pouvoir utiliser la fonction List.
+
--- SwiftUI Initiation/ContentView.swift
+++ SwiftUI Initiation/ContentView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    var loot = ["Epée", "Bouclier", "Armure"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(loot, id: \.self) { item in
+            Text(item)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
